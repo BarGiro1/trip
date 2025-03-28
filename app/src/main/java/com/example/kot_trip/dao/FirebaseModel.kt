558acb451a3a4d5420995c8413aeb978e19c6187
bargiro1 <76563bar@gmail.com>
2025-03-29 01:44:37 +0300

    add posts

---
+++ app/src/main/java/com/example/kot_trip/dao/FirebaseModel.kt
@@ -0,0 +1,101 @@
+package com.example.kot_trip.dao
+
+
+import android.graphics.Bitmap
+import android.util.Log
+import com.example.kot_trip.model.Post
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.firestore
+import com.google.firebase.firestore.firestoreSettings
+import com.google.firebase.firestore.memoryCacheSettings
+import com.google.firebase.storage.storage
+import java.io.ByteArrayOutputStream
+
+object FirebaseModel {
+    private val db = FirebaseFirestore.getInstance()
+    private val database = Firebase.firestore
+    private val storage = Firebase.storage
+
+    init {
+        val setting = firestoreSettings {
+            setLocalCacheSettings(memoryCacheSettings {  })
+        }
+
+        database.firestoreSettings = setting
+    }
+    fun addPost(post: Post, onSuccess: () -> Unit, onFailure: (Exception) -> Unit) {
+        db.collection("posts")
+            .document(post.id)
+            .set(post)
+            .addOnSuccessListener { onSuccess() }
+            .addOnFailureListener { onFailure(it) }
+    }
+
+    fun updatePost(post: Post, onSuccess: () -> Unit, onFailure: (Exception) -> Unit) {
+        db.collection("posts")
+            .document(post.id)
+            .set(post)
+            .addOnSuccessListener { onSuccess() }
+            .addOnFailureListener { onFailure(it) }
+    }
+
+    fun deletePost(postId: String, onSuccess: () -> Unit, onFailure: (Exception) -> Unit) {
+        db.collection("posts")
+            .document(postId)
+            .delete()
+            .addOnSuccessListener { onSuccess() }
+            .addOnFailureListener { onFailure(it) }
+    }
+
+    fun getAllPosts(onSuccess: (List<Post>) -> Unit, onFailure: (Exception) -> Unit) {
+        db.collection("posts")
+            .get()
+            .addOnSuccessListener { documents ->
+                val posts = documents.map { it.toObject(Post::class.java) }
+                onSuccess(posts)
+            }
+            .addOnFailureListener { onFailure(it) }
+    }
+
+    fun getPostsByCountryOrCity(country: String, city: String, onSuccess: (List<Post>) -> Unit, onFailure: (Exception) -> Unit) {
+        db.collection("posts")
+            .whereEqualTo("country", country)
+            .whereEqualTo("city", city)
+            .get()
+            .addOnSuccessListener { documents ->
+                val posts = documents.map { it.toObject(Post::class.java) }
+                onSuccess(posts)
+            }
+            .addOnFailureListener { onFailure(it) }
+    }
+
+    fun getPostsByUserId(userId: String, onSuccess: (List<Post>) -> Unit, onFailure: (Exception) -> Unit) {
+        db.collection("posts")
+            .whereEqualTo("userId", userId)
+            .get()
+            .addOnSuccessListener { documents ->
+                val posts = documents.map { it.toObject(Post::class.java) }
+                onSuccess(posts)
+            }
+            .addOnFailureListener { onFailure(it) }
+    }
+
+    fun uploadImage(image: Bitmap, name: String, callback: (String?) -> Unit) {
+        val storageRef = storage.reference
+        val imageProfileRef = storageRef.child("images/$name.jpg")
+        val baos = ByteArrayOutputStream()
+        image.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+        val data = baos.toByteArray()
+
+        val uploadTask = imageProfileRef.putBytes(data)
+        uploadTask
+            .addOnFailureListener { callback(null) }
+            .addOnSuccessListener { taskSnapshot ->
+                imageProfileRef.downloadUrl.addOnSuccessListener { uri ->
+                    callback(uri.toString())
+                }
+            }
+    }
+
+}
--- app/src/main/java/com/example/kot_trip/model/Post.kt
+++ app/src/main/java/com/example/kot_trip/model/Post.kt
@@ -3,8 +3,9 @@ package com.example.kot_trip.model
 data class Post(
     val id: String = "",
     val userId: String = "",
-    val city: String = "",
+    val title: String = "",
     val country: String = "",
+    val city: String = "",
     val description: String = "",
     val imageUrl: String = "",
     val timestamp: Long = System.currentTimeMillis()
