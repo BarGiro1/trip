1b98f9d42e9d871459cce9ef42a876b7ac1733db
bargiro1 <76563bar@gmail.com>
2025-03-28 22:49:55 +0300

    nav bar

---
+++ app/src/main/java/com/example/kot_trip/ui/post/AddPostFragment.kt
@@ -0,0 +1,10 @@
+package com.example.kot_trip.ui.post
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.kot_trip.R
+
+class AddPostFragment : Fragment(R.layout.fragment_add_post)
