18533afc5b36d3a52385754e7b8050163cc9903b
bargiro1 <76563bar@gmail.com>
2025-03-29 14:41:22 +0300

    add local db posts

---
+++ app/src/main/java/com/example/kot_trip/base/App.kt
@@ -0,0 +1,15 @@
+package com.example.kot_trip.base
+
+import android.app.Application
+import android.content.Context
+
+class App: Application() {
+
+    object Globals {
+        var context: Context? = null
+    }
+    override fun onCreate() {
+        super.onCreate()
+        Globals.context = applicationContext
+    }
+}
